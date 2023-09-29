4d57c2c9a7877a6d6039b0ece4871e570c4ea37d
Ayokunnumi1 <omololuayk@gmail.com>
2023-09-29 18:54:48 +0100

    Edit form input to include useState

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,27 +1,41 @@
-import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import { addBookToServer } from '../redux/books/bookSlice';
 
-const FormInput = ({ handleUserInput }) => {
+const FormInput = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    author: '',
+  });
+  const dispatch = useDispatch();
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    const title = e.target.elements.title.value;
-    const author = e.target.elements.author.value;
-    handleUserInput(title, author);
+    const { title, author } = formData;
+    if (title && author) {
+      dispatch(addBookToServer({
+        item_id: nanoid(), title, author, category: 'fiction',
+      }));
+      setFormData({
+        title: '',
+        author: '',
+      });
+    }
   };
-
   return (
-    <form onSubmit={handleSubmit} className="form-input">
-      <input type="text" name="title" placeholder="Book title" />
-
-      <input type="text" name="author" placeholder="author" />
-
-      <button type="submit">Add Book</button>
-
+    <form action="">
+      <input type="text" name="title" placeholder="Book title" className="" value={formData.title} onChange={handleInputChange} />
+      <input type="text" name="author" placeholder="author" className="" value={formData.author} onChange={handleInputChange} />
+      <button type="submit" className="" onClick={handleSubmit}>ADD BOOK</button>
     </form>
   );
-};
-
-FormInput.propTypes = {
-  handleUserInput: PropTypes.func.isRequired,
 };
 
 export default FormInput;
